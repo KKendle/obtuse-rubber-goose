b64f139b7e2b8ca04fbca602d90dc9ae52cb88c8
Kevin Kendle <kevin.kendle09@gmail.com>
2019-04-01 16:27:41 -0500

    add skip link and main element

--- header.php
+++ header.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-    <head>
-        <meta charset="<?php bloginfo('charset'); ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <?php wp_head(); ?>
-    </head>
-    <body <?php body_class(); ?>>
-        
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <a class="skip-to-main-content" href="#maincontent">Skip to main content</a>
+    <?php include_once "menu.php"; ?> 
